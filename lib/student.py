8b4fc5f8a07e7797227624356201067a7d1cd496
Moses-Oyelade <oyelademoses@gmail.com>
2023-10-12 06:54:51 +0100

    completed

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,29 @@
 class Student:
-    pass
+
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+        
+    def raise_hand(self):
+        print("Pick me!")
+        
+
+student1=Student()
+
+student1.hello()
+student1.raise_hand()
 
 class ChattyStudent(Student):
-    pass
+    
+    def hello(self):
+        Student.hello(self)
+        self.hello = ("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
+        print(self.hello)
+        
+    def raise_hand(self):
+        for i in range(10):
+            super().raise_hand()
+        
+special_stud = ChattyStudent()
+special_stud.__dict__
+special_stud.hello()
+special_stud.raise_hand()
